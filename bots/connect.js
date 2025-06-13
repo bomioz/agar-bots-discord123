82152fdabc02f840378e903a0d3c08f98cf2863a
bomioz <navarrochristopher068@gmail.com>
2025-06-13 01:40:23 -0400

    Update connect.js

--- bots/connect.js
+++ bots/connect.js
@@ -1,11 +1,25 @@
-const AgarClient = require('./lib/agario-client');
+const AgarioClient = require("../lib/agario-client");
+require("dotenv").config();
 
-function connectBots(partyCode, region, mode) {
-  const totalBots = 28;
+function connectBots(partyCode, region, mode, count) {
+  for (let i = 0; i < count; i++) {
+    const bot = new AgarioClient();
 
-  for (let i = 0; i < totalBots; i++) {
-    const bot = new AgarClient(partyCode, region, mode);
-    bot.connect();
+    bot.nick = process.env.NICKNAME || "Bot";
+    bot.connect(partyCode, region);
+
+    bot.on("connected", () => {
+      console.log(`${bot.nick} conectado`);
+      if (mode === "seguir") {
+        bot.setFollowMouse(true);
+      } else if (mode === "burst") {
+        bot.setBurstMode();
+      }
+    });
+
+    bot.on("error", (err) => {
+      console.error("Bot error:", err);
+    });
   }
 }
 
